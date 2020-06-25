e0070a933f7c66644fcd0ecb28baf2d372308fb1
aburd <burdick.aaron@gmail.com>
2020-06-25 13:30:57 +0900

    improve css :)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hangzonessss</h1>
+      <div className="app-body">
+        <h1>Hangzonessss</h1>
+      </div>
     </div>
   );
 }
