36aad0db074dd375ea75e03d6ff718faa4c1be4a
aburd <burdick.aaron@gmail.com>
2020-06-29 19:44:47 +0900

    make routes enum

---
+++ src/types/Routes.ts
@@ -0,0 +1,10 @@
+// This enum will tie the route itself and any links to those routes
+// You could use a reg JS object instead of TS enum, but the enum is nice
+// because if someone tries to mutate a property the compiler will get mad
+
+enum Routes {
+  Main = '/',
+  Login = '/login',
+}
+
+export default Routes
