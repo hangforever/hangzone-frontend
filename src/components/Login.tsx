32ec11834ef69ff92bc66759b8eb2129e2f199e1
aburd <burdick.aaron@gmail.com>
2020-07-02 13:52:05 +0900

    useHistory API

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router'
 import { FirebaseContext } from '../firebase'
 
 export default function Login() {
   const [email, updateEmail] = useState('')
   const [password, updatePassword] = useState('')
+  const history = useHistory()
   return (
     <FirebaseContext.Consumer>
       {firebase => { 
-        function login(email: string, password: string) {
+        function signUp(email: string, password: string) {
           return firebase
             .auth()
             .createUserWithEmailAndPassword(email, password)
+            .then(user => {
+              console.log('Successfully created user!', user)
+              history.push('/')
+            })
+            .catch(function (error) {
+              console.error(error.code, error.message)
+            })
+        }
+
+        function login(email: string, password: string) {
+          return firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(user => {
+              console.log('Successfully logged in!', user)
+              history.push('/')
+            })
             .catch(function (error) {
               console.error(error.code, error.message)
             })
@@ -18,6 +37,16 @@ export default function Login() {
           
         return (
           <div className="Login">
+            <h1>Sign up</h1>
+            <form onSubmit={e => {
+              e.preventDefault()
+              signUp(email, password)
+            }}>
+              <input type="text" name="email" value={email} onChange={e => updateEmail(e.target.value)} />
+              <input type="password" name="password" value={password} onChange={e => updatePassword(e.target.value)} />
+              <input type="submit" value="Submit" />
+            </form>
+            <h1>Login</h1>
             <form onSubmit={e => {
               e.preventDefault()
               login(email, password)
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,12 +7,12 @@ import "firebase/storage"
 
 // TODO: add potentially multiple configs for addition of STG/PRD servers
 var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig)
