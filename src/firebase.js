274c78987f1833d08e6c89953a18751817f7d908
aburd <burdick.aaron@gmail.com>
2020-07-02 11:00:28 +0900

    create class for firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage"
+
+var firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+export default class Firebase {
+  constructor() {
+    firebase.initializeApp(firebaseConfig)
+  }
+}
