db5dc40d40beb8e144fb470cfc8c320debd6b6d5
aburd <burdick.aaron@gmail.com>
2020-07-04 09:07:19 +0900

    change over to relative paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import './styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/main.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
