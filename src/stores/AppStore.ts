0fc0cf34d1a00183c24650efa34195770f827882
aburd <burdick.aaron@gmail.com>
2020-07-07 23:28:07 +0900

    fix really dumb type

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -4,12 +4,12 @@ import { Hangzone } from '../types'
 export default class AppStore {
   hangzones: Hangzone[] = observable([])
 
-  addTodo = action((name: string, description: string, isPrivate: boolean = true) => {
+  addHangzone = action((name: string, description: string, isPrivate: boolean = true) => {
     const hangzone = { id: this.hangzones.length.toString(), name, description, isPrivate }
     this.hangzones.push(hangzone)
   })
 
-  removeTodo = action((id: string) => {
+  removeHangzone = action((id: string) => {
     this.hangzones = this.hangzones.filter(h => h.id !== id)
   })
 }
