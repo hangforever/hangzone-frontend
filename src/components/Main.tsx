0571b6db2de676527c09fea8d7fa8ec1a543c759
aburd <burdick.aaron@gmail.com>
2020-07-09 22:05:35 +0900

    show example

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import sample from '../sample.png'
+import React, { useState, useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+import appStoreContext from '../stores/appStoreContext'
 
 // This is just a fake route that can be deleted whenever
 // it's just to demonstrate routing
-export default function Main() {
+function Main() {
+  const appStore = useContext(appStoreContext)
+  const [newWord, updateNewWord] = useState(appStore.wordOfTheDay.get())
   return (
     <div className="Main">
       <h1>Hangzonessss</h1>
-      <div className="pika">
-        <p>Image example using path</p>
-        <img src="/images/pikawho.png" alt="" />
-      </div>
-      <div className="sample">
-        <p>Image example using import</p>
-        <img src={sample} alt="" />
+      
+      <div>
+        <h2>The word of the day is</h2>
+        <h3>{appStore.wordOfTheDay.get()}</h3>
+
+        <div>But hey, you could always change it...</div>
+        <form onSubmit={e => {
+          e.preventDefault()
+          appStore.updateWordOfTheDay(newWord)
+        }}>
+          <input type="text" value={newWord} onChange={e => updateNewWord(e.target.value)} />
+        </form>
       </div>
       <div className="farts">
         <p>Now we're cookin with farts - Scott 2020 at the age of a single Willennium<br /><img src="https://upload.wikimedia.org/wikipedia/en/c/c1/WillSmith-Willennium.jpg" /></p>
@@ -21,3 +29,5 @@ export default function Main() {
     </div>
   )
 }
+
+export default observer(Main)
