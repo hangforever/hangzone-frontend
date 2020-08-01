1c4f69450a3fb6aa79fba5d2dbc1bb59f7af2740
aburd <burdick.aaron@gmail.com>
2020-08-02 03:12:55 +0900

    improve login screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,60 +1,59 @@
 import React, { useState, useContext } from 'react'
 import { useHistory } from 'react-router'
 import firebaseContext from 'firebaseContext'
+import { Routes } from 'types'
+import './Login.scss'
 
 const Login: React.SFC = () => {
   const [email, updateEmail] = useState('')
   const [password, updatePassword] = useState('')
   const history = useHistory()
   const firebase = useContext(firebaseContext)
+
+  function handleGmailLogin() {
+    const gAuthProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+      .signInWithPopup(gAuthProvider).then(() => history.push('/'))
+      .catch(function (error) {
+        console.error(error.code, error.message)
+      })
+  }
+
+  function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => history.push('/'))
+      .catch(function (error) {
+        console.error(error.code, error.message)
+      })
+  }
   
   return (
     <div className="Login">
-      <h1>Gmail</h1>
-      <button
-        onClick={() => {
-          const gAuthProvider = new firebase.auth.GoogleAuthProvider();
-          firebase.auth()
-            .signInWithPopup(gAuthProvider).then(() => history.push('/')).catch(function (error) {
-            console.error(error.code, error.message)
-          })
-        }}
-      >
-        Gmail
-      </button>
-      <h1>Sign up</h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          
-          firebase.auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then(() => history.push('/'))
-            .catch(function (error) {
-              console.error(error.code, error.message)
-            })
-        }}
-      >
-        <input type="text" name="email" value={email} onChange={e => updateEmail(e.target.value)} />
-        <input type="password" name="password" value={password} onChange={e => updatePassword(e.target.value)} />
-        <input type="submit" value="Submit" />
-      </form>
-      <h1>Login</h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          firebase.auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(() => history.push('/'))
-            .catch(function (error) {
-              console.error(error.code, error.message)
-            })
-        }}
-      >
-        <input type="text" name="email" value={email} onChange={e => updateEmail(e.target.value)} />
-        <input type="password" name="password" value={password} onChange={e => updatePassword(e.target.value)} />
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form
+          onSubmit={handleLogin}
+        >
+          <div className="form__inner">
+            <input type="text" name="email" value={email} onChange={e => updateEmail(e.target.value)} />
+            <input type="password" name="password" value={password} onChange={e => updatePassword(e.target.value)} />
+          </div>
+          <input className="btn--no-border" type="submit" value="LOG IN >>" />
+          <button
+            className="btn--no-border"
+            onClick={() => history.push(Routes.SignUp)}
+          >
+            Create Account >>
+          </button>
+          <button
+            className="btn--no-border"
+            onClick={handleGmailLogin}
+          >
+            Gmail >>
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
