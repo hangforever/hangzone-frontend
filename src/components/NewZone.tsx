6f2ddcdcbb8ec16ee4d4c0fb457880ba607913f1
scootyboots <scott.coffrin@gmail.com>
2020-08-08 20:30:50 +0900

    css for modal

--- src/components/NewZone.tsx
+++ src/components/NewZone.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import { observer } from "mobx-react-lite"
 import appStoreContext from '../stores/appStoreContext'
 import './NewZone.scss'
+import './modifiers.scss'
 
 const NewZoneModal = () => {
   const appStore = useContext(appStoreContext)
@@ -20,26 +21,28 @@ const NewZoneModal = () => {
       {active &&       
       <div className="NewZone__modal">
         <div className="NewZone__modal-container">
-          <div className="NewZone__modal-content">
-            <button className="NewZone__modal-close" onClick={() => toggleActive(active ? false : true)}>x</button>
-            <div className="form-item">
-              <label htmlFor="name">Name:</label>
-              <input type="text" value={name} name="name" onChange={e => updateName(e.target.value)} />
-            </div>
-            <div className="form-item">
-              <label htmlFor="is-private">Private?::</label>
-              <input type="checkbox" checked={isPrivate} name="is-private" onChange={e => updateIsPrivate(!isPrivate)} />
-            </div>
-            <div className="form-item">
-              <label htmlFor="description">Description:</label>
-              <textarea value={description} name="description" onChange={e => updateDescription(e.target.value)} />
-            </div>
-            <button onClick={() => {
-              appStore.addHangzone(name, description, isPrivate)
-              resetForm()
-              toggleActive(active ? false : true)
-            }}>complete</button>
-            </div>  
+            {/* <div className="NewZone__modal-close-container"> */}
+              <button className="NewZone__modal-close-x" onClick={() => toggleActive(active ? false : true)}>x</button>
+            {/* </div> */}
+            <div className="NewZone__modal-content">
+              <div className="form-item">
+                <label htmlFor="name">Name:</label>
+                <input type="text" value={name} name="name" onChange={e => updateName(e.target.value)} />
+              </div>
+              <div className="form-item">
+                <label className="--inline-block" htmlFor="is-private">Private?::</label>
+                <input type="checkbox" checked={isPrivate} name="is-private" onChange={e => updateIsPrivate(!isPrivate)} />
+              </div>
+              <div className="form-item">
+                <label htmlFor="description">Description:</label>
+                <textarea value={description} name="description" onChange={e => updateDescription(e.target.value)} />
+              </div>
+              <button onClick={() => {
+                appStore.addHangzone(name, description, isPrivate)
+                resetForm()
+                toggleActive(active ? false : true)
+              }}>complete</button>
+              </div>  
           </div>      
       </div>}
       {!active &&
