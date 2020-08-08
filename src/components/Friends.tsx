00871f4d48c4ef8faa3cd9a5550fe244c477910d
scootyboots <scott.coffrin@gmail.com>
2020-08-08 20:52:30 +0900

    basic friends content

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FriendsList from './FriendsList'
 import './Friends.scss'
 
 interface Props {
@@ -6,9 +7,33 @@ interface Props {
 }
 
 const Friends: React.SFC<Props> = () => {
+  const [search, updateSearch] = useState('')
+  function handleAddFriend() {
+    alert('Unimplemented!')
+  }
   return (
     <div className="Friends">
-      <h1>Friends List, bitch</h1>
+      <div className="Friends__search">
+        <input
+          type="text"
+          placeholder="search friends list"
+          className="input__search"
+          name="search"
+          value={search}
+          onChange={e => updateSearch(e.target.value)} 
+        />
+      </div>
+      <div className="Friends__friends-list">
+        <FriendsList />
+      </div>
+      <div className="Friends__controls">
+        <button 
+          className="button__add-friend"
+          onClick={handleAddFriend}
+        >
+          Add Friend
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/FriendsList.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {}
+
+const FriendsList: React.SFC<Props> = () => (
+  <div className="FriendsList">
+    <ul>
+      <li>Dan</li>
+      <li>NateDog</li>
+      <li>DP</li>
+      <li>Vish</li>
+    </ul>
+  </div>
+)
+
+export default FriendsList
