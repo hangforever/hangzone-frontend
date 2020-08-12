2ac2ebe53e3e91a4dc6e90f91f7653707398f904
aburd <burdick.aaron@gmail.com>
2020-08-13 00:49:15 +0900

    refactor fn

--- src/db/profiles.ts
+++ src/db/profiles.ts
@@ -1,16 +1,17 @@
 import { firebase } from 'firebaseContext'
 import { IProfile } from 'types'
 
-export async function createProfile(userUID: string, displayName: string, options: Partial<IProfile> = {}) {
+export async function createProfile(firebaseUserUID: string, displayName: string, options: Partial<IProfile> = {}): Promise<IProfile | undefined> {
   const profile = {
     displayName,
     friendIds: {},
     bio: options.bio || '',
     photoURL: options.photoURL || '/blank_hanger.png',
   }
-  const docRef = await firebase.firestore().collection('profiles').doc(userUID)
+  const docRef = await firebase.firestore().collection('profiles').doc(firebaseUserUID)
   await docRef.set(profile)
-  return docRef
+  const createdProfile = await docRef.get()
+  return createdProfile.data() as IProfile | undefined
 }
 
 export async function getProfile(firebaseUserUID: string): Promise<IProfile | null> {
