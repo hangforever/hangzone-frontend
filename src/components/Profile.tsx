04737b4bd3a6be1eeff02876e5e77c09c954826b
aburd <burdick.aaron@gmail.com>
2020-08-14 01:25:09 +0900

    setup basic upload code

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import { appStoreContext } from 'stores'
+import firebaseContext from 'firebaseContext'
 import { setProfile } from 'db/profiles'
 import { NavLink } from 'react-router-dom'
 import Field from 'components/Field'
@@ -10,15 +11,43 @@ import './Profile.scss'
 
 const Profile = () => {
   const appStore = useContext(appStoreContext)
+  const firebase = useContext(firebaseContext)
+  const { firebaseUser, profile } = appStore
   const [modalActive, updateModalActive] = useState(false)
+  const [uploadProgress, updateUploadProgress] = useState(0)
 
   function handleUpgradeAccount() {
     alert('Unimplemented')
     console.log('Docs for upgrading here: https://firebase.google.com/docs/auth/web/anonymous-auth#convert-an-anonymous-account-to-a-permanent-account')
   }
-  
+  async function handleImageUpload(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files && e.target.files[0]
+    try {
+      if (!file) throw new Error('There was a problem getting your profile image.')
 
-  const { firebaseUser, profile } = appStore
+      const storageRef = firebase.storage().ref()
+      const userProfileImgRef = storageRef.child(`profile/images/${appStore.firebaseUser?.uid}`)
+      const uploadTask = userProfileImgRef.put(file)
+
+      // https://firebase.google.com/docs/storage/web/upload-files#full_example
+      uploadTask.on('state_changed', function (snapshot) {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        updateUploadProgress(progress)
+      }, function (error) {
+        alert(error.message)
+      }, function () {
+        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+          if (appStore.profile && appStore.firebaseUser) {
+            appStore.profile.photoURL = downloadURL
+            setProfile(appStore.firebaseUser.uid, appStore.profile)
+            updateModalActive(false)
+          }
+        });
+      });
+    } catch (e) {
+      alert(e.message)
+    }
+  }
   
   return firebaseUser && profile ? (
     <div className="Profile">
@@ -36,7 +65,13 @@ const Profile = () => {
           }}
         />
         <div>Or upload a file</div>
-        <input type="file" name="" id=""/>
+        <input
+          type="file"
+          accept="image/*"
+          name="profile-upload"
+          id="profile-upload"
+          onChange={handleImageUpload}
+        />
       </Modal>
       <div>
         {firebaseUser.isAnonymous ? (
