f5d3eeb5f2d84f16afea5d1165b86360c8dfe510
scootyboots <scott.coffrin@gmail.com>
2020-08-17 19:30:48 +0900

    first commit

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { appStoreContext } from 'stores'
+import firebaseContext from 'firebaseContext'
 import { IFriend } from '../types'
 import StatusMark from './StatusMark'
 import './FriendsList.scss'
@@ -7,20 +9,24 @@ interface Props {
   friends: IFriend[]
 }
 
-const FriendsList: React.SFC<Props> = ({ friends }) => (
-  <div className="FriendsList">
-    <ul>
-      {friends.map(friend => 
-        <li className="FriendsList__friend">
-          <div className="FriendsList__friend-photo">
-            <img src={friend.photoURL} alt="profile"/>
-          </div>
-          <div className="FriendsList__friend-display-name">{friend.displayName}</div>
-          <div className="FriendsList__friend-status">{friend.status}</div>
-          <StatusMark status={friend.status} />
-        </li>)}
-    </ul>
-  </div>
-)
+const FriendsList: React.SFC<Props> = ({ friends }) => {
+  const appStore = useContext(appStoreContext)
+  const firebase = useContext(firebaseContext)
+  console.log(appStore)
+  return (
+    <div className="FriendsList">
+      <ul>
+        {friends.map(friend => 
+          <li className="FriendsList__friend">
+            <div className="FriendsList__friend-photo">
+              <img src={friend.photoURL} alt="profile"/>
+            </div>
+            <div className="FriendsList__friend-display-name">{friend.displayName}</div>
+            <div className="FriendsList__friend-status">{friend.status}</div>
+            <StatusMark status={friend.status} />
+          </li>)}
+      </ul>
+    </div>
+)}
 
 export default FriendsList
