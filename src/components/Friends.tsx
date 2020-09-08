a171028795afccc6b549332c1f972b08ea346181
scootyboots <scott.coffrin@gmail.com>
2020-09-08 19:49:29 +0900

    remove some comments and console logs

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -24,41 +24,19 @@ const Friends: React.SFC<Props> = () => {
     return searchRegex.test(cur.displayName)
   })
 
-  console.log('friendState', friendProfileState)
-  console.log('friend profiles from store' , friendProfiles)
-  console.log(friendProfiles[0].displayName)
-  console.log('im dumb', profile?.friendIds)
-
-
   const handleAddFriend = () => {
-    // if (firebaseUser && profile) { 
-    //   const idToAdd: string | null = prompt('add this friend id')
-    //   profile.friendIds = {...profile.friendIds, [idToAdd]: true}
-    //   console.log(profile.friendIds)
-    // }
+    /***
+     TODO: handle adding new friends, here's an idea
+     ****
+      if (firebaseUser && profile) { 
+      const idToAdd: string | null = prompt('add this friend id')
+      profile.friendIds = {...profile.friendIds, [idToAdd]: true}
+      console.log(profile.friendIds)
+    }
+     */
   }
 
-
-  // useEffect(() => {
-  //   if (profile) {
-  //     1. get all UIDs necessary to contact DB
-  //     const friendUids = Object.keys(profile.friendIds)
-  //     console.log(friendUids)
-  //     if(friendUids.length > 0) {
-  //       getFriendProfiles(friendUids).then(friendProfiles => updateFriendProfiles(friendProfiles))
-  //     }
-
-  //     2. use those IDs to perform a query for profiles on the DB
-  //       made getFriendProfiles function in db/profiles 
-  //       called in index.tsx, manually passing string for testing
-
-  //     3. Take that result and set the friends state of the store
-  //       attempted to add friendProfiles to appStor
-  //     console.log('friendProfiles:', friendProfileState)
-  //   }
-  // }, [profile, friendProfileState])
-
-  return firebaseUser && profile && friendProfiles ? (
+  return firebaseUser && profile && friendProfiles.length > 0 ? (
     <div className="Friends">
       <div className="Friends__search">
         <input
