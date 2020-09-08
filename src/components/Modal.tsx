dba6db065be4b7541550299a5ea7c85b025e93d2
scootyboots <scott.coffrin@gmail.com>
2020-09-08 20:49:40 +0900

    add maxHeight prop

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,6 +5,7 @@ interface Props {
   active: boolean,
   className?: string,
   maxHeight?: string,
+  maxWidth?: string,
   onCloseClick: () => void
 }
 
@@ -12,12 +13,13 @@ const Modal: React.SFC<Props> = ({
   active,
   className = '',
   maxHeight = '',
+  maxWidth = '',
   onCloseClick,
   children,
 }) => {
   return active ? (
     <div className="Modal">
-      <div className={`Modal__container ${className}`}>
+      <div className={`Modal__container ${className}`} style={{maxWidth: maxWidth}}>
         <div className="Modal__close-x" onClick={onCloseClick}>x</div>
         <div className="Modal__content" style={{maxHeight: maxHeight}}>
           {children}
