1e23f8c7d1b1cda7fcf1a94a0afe75fc05fa1273
aburd <burdick.aaron@gmail.com>
2020-11-29 01:40:59 +0900

    add types

---
+++ functions/src/types/index.ts
@@ -0,0 +1,25 @@
+export interface Hangzone {
+  id: string;
+  name: string;
+  isPrivate: boolean;
+  description: string;
+}
+
+export interface ISettings {
+  gpsOn: boolean;
+  emailOnFriendHang: boolean;
+  notifications: boolean;
+}
+
+export interface IProfile {
+  displayName: string;
+  friendIds: { [userId: string]: boolean };
+  bio?: string;
+  photoURL?: string;
+}
+
+export enum Status {
+  Active = 'active',
+  Inactive = 'inactive',
+  Offline = 'offline',
+}
