b803e8df047ad1f27f91f01074b3ae93f3028aac
aburd <burdick.aaron@gmail.com>
2020-11-29 01:41:03 +0900

    add profile fn

--- functions/src/profiles.ts
+++ functions/src/profiles.ts
@@ -1,36 +1,43 @@
 import { Router } from 'express';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { IProfile } from './types';
 
 const router = Router();
 
-router
-  .get('/:id', (req, res) => {
-    functions.logger.log('User requested the profile with ID:', req.params.id);
-    res.send('OK');
-  })
-  .post('/', (req, res) => {
-    functions.logger.log('User requested to create a profile');
-    res.send('OK');
-  });
+router.get('/:id', (req, res) => {
+  functions.logger.log('User requested the profile with ID:', req.params.id);
+  res.send('OK');
+});
 
-// export async function createProfile(
-//   firebaseUserUID: string,
-//   displayName: string,
-//   options: Partial<IProfile> = {}
-// ): Promise<IProfile | undefined> {
-//   const profile = {
-//     displayName,
-//     friendIds: {},
-//     bio: options.bio || '',
-//     photoURL: options.photoURL || '/blank_hanger.png',
-//   };
-//   const docRef = await firebase
-//     .firestore()
-//     .collection('profiles')
-//     .doc(firebaseUserUID);
-//   await docRef.set(profile);
-//   const createdProfile = await docRef.get();
-//   return createdProfile.data() as IProfile | undefined;
-// }
+router.post('/', async (req, res) => {
+  const { id, name } = req.body;
+  if (!(id || name)) {
+    return res.send('Error');
+  }
+
+  await createProfile(id, name);
+  return res.send('OK');
+});
+
+export async function createProfile(
+  firebaseUserUID: string,
+  displayName: string,
+  options: Partial<IProfile> = {}
+): Promise<IProfile | undefined> {
+  const profile = {
+    displayName,
+    friendIds: {},
+    bio: options.bio || '',
+    photoURL: options.photoURL || '/blank_hanger.png',
+  };
+  const docRef = await admin
+    .firestore()
+    .collection('profiles')
+    .doc(firebaseUserUID);
+  await docRef.set(profile);
+  const createdProfile = await docRef.get();
+  return createdProfile.data() as IProfile | undefined;
+}
 
 export default router;
