54d2fc5eaf20487a0a94acf69f2a28a40998d867
aburd <burdick.aaron@gmail.com>
2020-12-22 00:21:47 +0900

    add hidden debugzone

---
+++ src/components/DebugZone.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * A General debug sandbox that only shows up in local development
+ * You can do anything in here! (I'm too lazy to add storybook)
+ */
+export default function DebugZone() {
+  return (
+    <div className="DebugZone">
+      <h1>The freedom to do anything isn't freedom at all!</h1>
+      <div className="DebugZone-body">The body</div>
+    </div>
+  );
+}
