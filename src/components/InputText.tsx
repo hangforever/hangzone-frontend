ad2ddf4d5f154bd19b1054bc47a36f3969915b4b
aburd <burdick.aaron@gmail.com>
2021-01-10 21:32:48 +0900

    add classname

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -6,9 +6,14 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   solid?: boolean;
 }
 
-export default function InputText({ iconGlyph, solid, ...rest }: Props) {
+export default function InputText({
+  className = '',
+  iconGlyph,
+  solid,
+  ...rest
+}: Props) {
   return (
-    <div className={`InputText ${solid ? 'solid' : ''}`}>
+    <div className={`InputText ${solid ? 'solid' : ''} ${className}`}>
       <input className={iconGlyph ? 'has-icon' : ''} type="text" {...rest} />
       <div className="shadow"></div>
       {iconGlyph && <span className="icon oi" data-glyph={iconGlyph} />}
