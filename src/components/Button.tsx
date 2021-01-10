71648a4504dd8c0451bf3f0df17ad5334086d926
aburd <burdick.aaron@gmail.com>
2021-01-10 21:16:43 +0900

    add button functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { ToolColor } from 'types';
 import './Button.scss';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  faceColor?: ToolColor;
+  shadowColor?: ToolColor;
   iconGlyph?: string;
   solid?: boolean;
 }
 
-export default function Button({ iconGlyph, solid, ...rest }: Props) {
+export default function Button({
+  iconGlyph,
+  faceColor = 'black',
+  shadowColor = 'green',
+  children,
+  ...rest
+}: Props) {
+  const backgroundColor = (color: ToolColor) => `background-${color}`;
   return (
-    <div className={`Button ${solid ? 'solid' : ''}`}>
-      <button className={iconGlyph ? 'has-icon' : ''} {...rest} />
-      <div className="shadow"></div>
+    <div className={`Button`}>
+      <button
+        className={`${iconGlyph ? 'has-icon' : ''} ${backgroundColor(
+          faceColor
+        )}`}
+        {...rest}
+      >
+        {children}
+      </button>
+      <div className={`shadow ${backgroundColor(shadowColor)}`}></div>
       {iconGlyph && <span className="icon oi" data-glyph={iconGlyph} />}
     </div>
   );
