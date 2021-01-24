5c37690d28aaf20db3b09bdb329a25e33b72c22e
aburd <burdick.aaron@gmail.com>
2021-01-24 12:24:21 +0900

    eslint

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { useHistory } from 'react-router';
 import firebaseContext from 'firebaseContext';
 import API from 'api/axios';
 import InputText from './InputText';
